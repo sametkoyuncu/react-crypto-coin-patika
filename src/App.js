503330506f39754d4e0b7beaa3ab1042c2d658e7
sametkoyuncu <sametkoyuncu@live.com>
2022-01-13 22:40:27 +0300

    Add static table component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Table from './components/Table'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen w-full pt-10 bg-green-300">
+      <div className="container mx-auto p-5 bg-green-300">
+        <Table />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
